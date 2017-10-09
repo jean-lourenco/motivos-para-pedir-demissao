b821c2bb6ef5f05ad557df8122becc5a1d45965a
Fabricio dos Santos Antunes <fabriciodossa@hotmail.com>
2017-10-09 14:47:48 -0300

    Adicionando motivos reeais
    Sério, é tudo motivo real.

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ let motivos = [
 	'Acabou o leite do café',
 	'Acabou o açucar do café',
 	'Nunca posso nada nessa p****!',
-	'Demissão não se pede, se ganha!'
+	'Demissão não se pede, se ganha!',
+	'A empresa usa Sybase',
+	'O banco está com problema e o dba não resolve',
+	'É sempre culpa da aplicação, nunca do banco'
 ];
 
 document.getElementById('motivo').firstChild.innerText = motivos[Math.trunc(Math.random() * motivos.length)];
