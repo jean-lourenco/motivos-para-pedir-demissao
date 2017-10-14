0f3cda56480ed7262774165c654b3ef4190a1744
Jean Lourenço <jeancarlos.bnu@gmail.com>
2017-10-14 12:46:09 -0300

    alterado disposição de vírgulas para facilitar merge

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 let motivos = [
-	'No começo era legal, mas agora eu quero matar todo mundo',
-	'Uma capivara me disse pra fazer isso',
-	'Papai quer que eu faça medicina',
-	'Acabou o café',
-	'Acabou o leite do café',
-	'Acabou o açucar do café',
-	'Nunca posso nada nessa p****!',
-	'Demissão não se pede, se ganha!',
-	'A empresa usa Sybase',
-	'O comercial vendeu uma feature que não existe no sistema', 
-	'Agendaram a homologação do sistema para às 18h de sexta',
-	'Meu trabalho esta  matando a minha criatividade',
-	'Patch de correção para um problema que foi criado por um patch anterior',
-	'Deixaram pra contratar a consultoria quando começou a dar merda',
-	'O planejamento do projeto se resume a fazer tudo no tempo',
-	'Não entendi a change, mas os totais voltaram a bater'
+	'No começo era legal, mas agora eu quero matar todo mundo'
+	, 'Uma capivara me disse pra fazer isso'
+	, 'Papai quer que eu faça medicina'
+	, 'Acabou o café'
+	, 'Acabou o leite do café'
+	, 'Acabou o açucar do café'
+	, 'Nunca posso nada nessa p****!'
+	, 'Demissão não se pede se ganha!'
+	, 'A empresa usa Sybase'
+	, 'O comercial vendeu uma feature que não existe no sistema' 
+	, 'Agendaram a homologação do sistema para às 18h de sexta'
+	, 'Meu trabalho esta  matando a minha criatividade'
+	, 'Patch de correção para um problema que foi criado por um patch anterior'
+	, 'Deixaram pra contratar a consultoria quando começou a dar merda'
+	, 'O planejamento do projeto se resume a fazer tudo no tempo'
+	, 'Não entendi a change, mas os totais voltaram a bater'
 ];
 
 document.getElementById('motivo').firstChild.innerText = motivos[Math.trunc(Math.random() * motivos.length)];
