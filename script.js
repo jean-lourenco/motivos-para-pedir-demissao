a2581642ff4b7e008fa5450c39894a864f356a86
= <ubuntu@gustavo.localdomain>
2017-10-14 13:27:05 +0000

    novo motivo

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ let motivos = [
 	'Demissão não se pede, se ganha!',
 	'A empresa usa Sybase',
 	'O comercial vendeu uma feature que não existe no sistema', 
-	'Agendaram a homologação do sistema para às 18h de sexta'
+	'Agendaram a homologação do sistema para às 18h de sexta',
+	'Não entendi a change, mas os totais voltaram a bater'
 ];
 
 document.getElementById('motivo').firstChild.innerText = motivos[Math.trunc(Math.random() * motivos.length)];
