8c1c1d8728eb2473d8f097403330ce0ecb4dafc5
Ismael S. Kafeltz <ismael.kafeltz@pagueveloz.com.br>
2019-06-27 14:22:36 -0300

    correções e novas mensagens

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let motivos = [
 	'No começo era legal, mas agora eu quero matar todo mundo',
 	'Acabou o café',
 	'Acabou o leite do café',
-	'Acabou o açucar do café',
+	'Acabou o açúcar do café',
 	'Nunca posso nada nessa p****!',
 	'Demissão não se pede se ganha!',
 	'A empresa usa Sybase',
 	'O comercial vendeu uma feature que não existe no sistema' ,
 	'Agendaram a homologação do sistema para às 18h de sexta',
-	'Meu trabalho esta  matando a minha criatividade',
+	'Meu trabalho esta matando a minha criatividade',
 	'Patch de correção para um problema que foi criado por um patch anterior',
 	'Deixaram pra contratar a consultoria quando começou a dar merda',
 	'O planejamento do projeto se resume a fazer tudo no tempo',
@@ -21,6 +21,9 @@ let motivos = [
 	'A Senior comprou a empresa',
 	'A empresa usa Delphi e Firebird (Quem que perde tempo com isso?)',
 	'O banheiro fede',
+	'Ninguém tem tempo, o tempo todo.',
+	'Recebi outro projeto que pediram a conta antes de terminá-lo.',
+	'Tá todo mundo pedindo a conta já no meu primeiro dia de trabalho.'
 ];
 
 document.getElementById('motivo').firstChild.innerText = motivos[Math.trunc(Math.random() * motivos.length)];
