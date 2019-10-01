eda8c81fcf5dcd3cfda2bdd35d75c4b123d41889
Janderson Constantino <jandersonconstantino@gmail.com>
2019-10-01 09:32:16 -0300

    Adding a new phrase abour AngularJS performance. (#19)
    * Add a new phrase.
    
    * Update script.js

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ let motivos = [
 	'Não alinharam minha expectativa.',
 	'Não posso mais pegar férias.',
 	'Houve uma proposta irrecusável de uma empresa que tem sede nova decente, além de outros benefícios como: possibilidade de trabalhar remoto e caixa de Budweiser a menos de 10 pila.',
+	'Utilizam AngularJS e reclamam de performance.',
 ];
 
 document.getElementById('motivo').firstChild.innerText = motivos[Math.trunc(Math.random() * motivos.length)];
